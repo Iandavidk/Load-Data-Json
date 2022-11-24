99489216bcd21c9d21f9436b33c894cbd0aaa4cf
Iandavidk <eandavid6@gmail.com>
2022-11-24 22:32:58 +0300

    fetch: fetch json data

---
+++ main.js
@@ -0,0 +1,6 @@
+async function loadIntoTable(url, table) {
+    const tableHead = table.querySelector("thead")
+    const tableBody = table.querySelector("tbody")
+    const response = await fetch(url)
+    const {headers, rows} = await response.json()
+}
